ea09f1b1631ae2de6979ca4ace0a3a574ee2a76c
Bongani <Bongtheman@outlook.com>
2021-06-09 11:13:39 +0100

    Adding files to group repo

---
+++ APITest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+namespace APIService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
